56061236ad78ef0bc4f2990a29e687361d0c1fa2
syeddelwar <delwar.mmc@gmail.com>
2023-01-29 01:09:02 -0500

    frontend

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ export const API_URL =
   process.env.API_URL || "https://print-graphbackend-production.up.railway.app";
 
 export const NEXT_URL =
-  process.env.NEXTJS_PUBLIC_FRONTEND_URL || `https://paintsme-production.up.railway.app`;
+  process.env.NEXTJS_PUBLIC_FRONTEND_URL || `print-graphfrontend-production.up.railway.app`;
 
 // `https://paintsme-production.up.railway.app`
 // `http://localhost:3000`
